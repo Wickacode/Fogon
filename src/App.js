ad981c818e0b6c627aa6c98381781f2727f32e04
Jessica Garrido <jessica.garrido.di@gmail.com>
2023-08-24 16:58:22 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
 
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import FogonLogo from '../assets/fogon-img/fogon.png'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className='navbar'>
+        <img src={FogonLogo} alt="" />
+        <ul>
+            <li>Accueil</li>
+            <li>Menu</li>
+            <li>Réservation</li>
+            <li>Contact</li>
+            
+            <span className='contact-number'>
+        <i class="fa-solid fa-phone"></i>
+        05 59 01 70 95
+        </span>
+        </ul>
+        
+      </nav>
+    </div>
+  )
+}
